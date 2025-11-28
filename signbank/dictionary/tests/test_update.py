75429b352b2b6a721610d074d78c1b74d3d8fd11
jonholdsworth <82071930+jonholdsworth@users.noreply.github.com>
2025-11-28 11:59:33 +1100

    Revert "Test updated"
    This reverts commit f2009a2935b91a85377bad26083024793b684a6e.

--- signbank/dictionary/tests/test_update.py
+++ signbank/dictionary/tests/test_update.py
@@ -126,9 +126,9 @@ class UpdateGlossTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         glossvid.refresh_from_db()
         self.assertEqual(glossvid.title, new_title)
-        if os.path.isfile(glossvid.videofile.name):
+        if os.path.isfile(glossvid.videofile.path):
             # Remove the file.
-            os.remove(glossvid.videofile.name)
+            os.remove(glossvid.videofile.path)
 
         # Test updating title for nonexisting glossvideo.
         response = self.client.post(reverse('dictionary:update_gloss', args=[self.testgloss.pk]),
