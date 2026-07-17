d8e4c3e30daecb7934b36b9d293f78f74f104013
jonholdsworth <82071930+jonholdsworth@users.noreply.github.com>
2026-07-17 16:29:43 +1000

    Reverting templates, trying JS instead

--- signbank/static/js/gloss_edit.js
+++ signbank/static/js/gloss_edit.js
@@ -168,6 +168,10 @@ $.editable.addInputType('multicheckbox', {
 
 function configure_edit() {
 
+    function trim_before(e) {
+        e.target.innerText = e.target.innerText.trim();
+    }
+
     $.fn.editable.defaults['indicator'] = 'Saving...';
     $.fn.editable.defaults['tooltip'] = 'Click to edit...';
     $.fn.editable.defaults['placeholder'] = '-';
@@ -183,7 +187,9 @@ function configure_edit() {
                         };
 
 
-     $('.edit_text').editable(edit_post_url);
+     $('.edit_text').editable(edit_post_url, {
+         before    : trim_before,
+     });
      $('.edit_int').editable(edit_post_url, {
          type      : 'positiveinteger',
          onerror : function(settings, original, xhr){
@@ -192,19 +198,22 @@ function configure_edit() {
                     },
      });
      $('.edit_area').editable(edit_post_url, {
-         type      : 'textarea'
+         type      : 'textarea',
+         before    : trim_before,
      });
      $('.edit_area_translations').editable(edit_post_url, {
          type      : 'textarea',
          width     : 400,
          rows      : 3,
          onblur    : 'ignore',
+         before    : trim_before,
      });
      $('.edit_area_notes').editable(edit_post_url, {
          type      : 'textarea',
          width     : 400,
          rows      : 3,
          onblur    : 'ignore',
+         before    : trim_before,
      });
      $('.edit_url').editable(edit_post_url, {
          type      : 'text',
